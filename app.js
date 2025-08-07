d203fa1427c3521e6265a04e490f0bdaf2d1d4ec
alenprastya06 <alen@seleris.id>
2025-08-07 11:47:32 +0700

    update cors

--- app.js
+++ app.js
@@ -12,29 +12,11 @@ const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// Static files
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-// Routes
 app.use("/api", profilRoutes);
 app.use("/api/kelengkapan", kelengkapanData);
 app.use("/api/auth", authRoutes);
 app.use("/api/documents", documentRoutes);
-
-// COMMENT ERROR HANDLING CORS INI JUGA
-/*
-app.use((error, req, res, next) => {
-  if (error.message === "Not allowed by CORS") {
-    res.status(403).json({
-      error: "CORS Error",
-      message: "Origin not allowed",
-    });
-  } else {
-    next(error);
-  }
-});
-*/
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
